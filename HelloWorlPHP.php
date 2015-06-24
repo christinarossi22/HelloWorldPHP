07ee34c30d116ce3b61f4d7aacda7d83fa81cbb6
christinarossi22 <christinarossi22@gmail.com>
2015-06-24 16:24:15 -0500

    Update HelloWorlPHP.php

--- HelloWorlPHP.php
+++ HelloWorlPHP.php
@@ -1,13 +1,13 @@
 
 <?php
 //opening php tag
-//for each number from 1 - 100 (store in veriable $i) opening foreach tag
+//for each number from 1 - 100 (store in variable $i) opening foreach tag
 foreach(range(1,100) as $i) {
 	
-//declaring veriable val to store the calculations
-// where the number 1-100 is a multiple of 3  print Hello..(then concatinate second condition).. where the number 1-100 is a multiple of 5  print World
+//declaring variable val to store the calculations
+// where the number 1-100 is a multiple of 3  print Hello..(then concatenate second condition).. where the number 1-100 is a multiple of 5  print World
   $val = ($i % 3 == 0 ? "Hello" : "").($i % 5 == 0 ? "World" : "");
-  // print nothing next to unbers where it is not a multiple of 3 or 5 or both. then "break" to go to next line.
+  // print nothing next to nunbers where it is not a multiple of 3 or 5 or both. then "break" to go to next line.
   echo (empty($val) ? $i : $val) .  '<br />';
 //closing foreach tag
 }
